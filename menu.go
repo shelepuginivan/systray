79e57c04999fdf02bf4c1f1c792ab44f33b0c369
shelepuginivan <shelepuginivanm@gmail.com>
2025-06-24 15:58:32 +0300

    feat(menu): add helper wrappers around `Event`

--- menu.go
+++ menu.go
@@ -2,6 +2,7 @@ package systray
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/godbus/dbus/v5"
 )
@@ -67,6 +68,14 @@ func (m *Menu) GetLayout(parentID int, recursionDepth int, propertyNames []strin
 	}
 
 	return revision, menu, nil
+}
+
+func (m *Menu) Clicked(target *LayoutNode) error {
+	return m.Event(target.ID, "clicked", 0, uint32(time.Now().Unix()))
+}
+
+func (m *Menu) Hovered(target *LayoutNode) error {
+	return m.Event(target.ID, "hovered", 0, uint32(time.Now().Unix()))
 }
 
 func (m *Menu) Event(targetID int32, eventID string, data any, timestamp uint32) error {

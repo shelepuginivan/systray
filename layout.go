f2eae2839fb49843d691c5b63be5bbdf87a681bd
shelepuginivan <shelepuginivanm@gmail.com>
2025-06-25 07:08:32 +0300

    feat(layout): add helper wrappers for node properties

--- layout.go
+++ layout.go
@@ -6,6 +6,35 @@ import (
 	"github.com/godbus/dbus/v5"
 )
 
+type LayoutNodeToggleType string
+
+// [LayoutNode] toggle types.
+const (
+	LayoutNodeToggleTypeNone      LayoutNodeToggleType = ""
+	LayoutNodeToggleTypeCheckmark LayoutNodeToggleType = "checkmark"
+	LayoutNodeToggleTypeRadio     LayoutNodeToggleType = "radio"
+)
+
+type LayoutNodeToggleState int
+
+// [LayoutNode] toggle states.
+const (
+	LayoutNodeToggleStateIndeterminate LayoutNodeToggleState = iota - 1
+	LayoutNodeToggleStateOff
+	LayoutNodeToggleStateOn
+)
+
+func (ts LayoutNodeToggleState) String() string {
+	switch ts {
+	case LayoutNodeToggleStateOff:
+		return "off"
+	case LayoutNodeToggleStateOn:
+		return "on"
+	default:
+		return "indeterminate"
+	}
+}
+
 // LayoutNode represents entry of the menu layout.
 //
 // The menu layout is recursive.
@@ -57,4 +86,119 @@ func NewLayoutNode(data any) (*LayoutNode, error) {
 	}
 
 	return root, nil
+}
+
+// IsEnabled reports whether layout node can be the target of events, e.g. can
+// be clicked or hovered.
+func (node *LayoutNode) IsEnabled() bool {
+	enabled, exists := node.Properties["enabled"]
+	if !exists {
+		return true // Enabled by default.
+	}
+	return enabled == true
+}
+
+// IsSeparator reports whether layout node is a separator.
+func (node *LayoutNode) IsSeparator() bool {
+	nodeType, exists := node.Properties["type"]
+	return exists && nodeType == "separator"
+}
+
+// IsSubmenu reports whether layout node is a submenu.
+func (node *LayoutNode) IsSubmenu() bool {
+	childrenDisplay, exists := node.Properties["children-display"]
+	return exists && childrenDisplay == "submenu"
+}
+
+// IsVisible reports whether layout node is visible in the menu.
+func (node *LayoutNode) IsVisible() bool {
+	visible, exists := node.Properties["visible"]
+	if !exists {
+		return true // Visible by default.
+	}
+	return visible == true
+}
+
+// Label returns text label of layout node if exists.
+//
+//   - Two consecutive underscore characters "__" should be displayed as a
+//     single underscore.
+//   - Any remaining underscore characters should not displayed at all, the
+//     first of those remaining underscore characters (unless it is the last
+//     character in the string) indicates that the following character is the
+//     access key.
+func (node *LayoutNode) Label() string {
+	nodeLabel, exists := node.Properties["label"]
+	if !exists {
+		return ""
+	}
+
+	label, ok := nodeLabel.(string)
+	if !ok {
+		return ""
+	}
+
+	return label
+}
+
+// IconName returns name of the icon, following the
+// [Freedesktop Icon Naming Specification].
+//
+// [Freedesktop Icon Naming Specification]: https://specifications.freedesktop.org/icon-naming-spec/latest/
+func (node *LayoutNode) IconName() string {
+	nodeIconName, exists := node.Properties["icon-name"]
+	if !exists {
+		return ""
+	}
+
+	iconName, ok := nodeIconName.(string)
+	if !ok {
+		return ""
+	}
+
+	return iconName
+}
+
+// IconData returns PNG bytes of the custom icon.
+func (node *LayoutNode) IconData() []byte {
+	nodeIconData, exists := node.Properties["icon-data"]
+	if !exists {
+		return nil
+	}
+
+	iconData, ok := nodeIconData.([]byte)
+	if !ok {
+		return nil
+	}
+
+	return iconData
+}
+
+// ToggleType returns toggle type of the layout node.
+func (node *LayoutNode) ToggleType() LayoutNodeToggleType {
+	switch node.Properties["toggle-type"] {
+	case "checkmark":
+		return LayoutNodeToggleTypeCheckmark
+	case "radio":
+		return LayoutNodeToggleTypeRadio
+	default:
+		return LayoutNodeToggleTypeNone
+	}
+}
+
+// ToggleState describes the current state of a togglable item.
+//
+// The implementation does not itself handle ensuring that only one item in a
+// radio group is set to "on", or that a group does not have "on" and
+// "indeterminate" items simultaneously; maintaining this policy is up to the
+// toolkit wrappers.
+func (node *LayoutNode) ToggleState() LayoutNodeToggleState {
+	switch node.Properties["toggle-state"] {
+	case 0, int32(0):
+		return LayoutNodeToggleStateOff
+	case 1, int32(1):
+		return LayoutNodeToggleStateOn
+	default:
+		return LayoutNodeToggleStateIndeterminate
+	}
 }
